68a44f267f364cb3b96e027387a327730bca91d9
vidigummy <vidigummy@gmail.com>
2021-02-14 22:16:55 +0900

    dsa

--- yo/server.js
+++ yo/server.js
@@ -1,31 +1,12 @@
 var http = require('http');
 var fs = require('fs');
+var express = require('express');
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'index')))
 
 function send404Message(response) {
     response.writeHead(404, { "Content-Type": "text/plain" });
     response.write("404 ERROR...");
     response.end();
 }
-
-function onRequest(request, response) {
-
-    if (request.method == 'GET') {
-        console.log(request.url);
-        if (request.url == '/' || request.url == "/index.html") {
-            response.writeHead(200, { "Context-Type": "text/html" });
-            fs.createReadStream("./index.html").pipe(response);
-        } else if (request.url == "/a.html") {
-            console.log(1);
-            response.writeHead(200, { "Context-Type": "text/html" });
-            fs.createReadStream("./a.html").pipe(response);
-        }
-    } else {
-        send404Message(response);
-    }
-
-}
-
-
-
-http.createServer(onRequest).listen(3000);
-console.log("Server");
