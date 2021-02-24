b235769443c0ce4e6635c720db3f6910171317a5
vidigummy <vidigummy@gmail.com>
2021-02-24 23:27:47 +0900

    씨발 왜 안돼

--- yo/models/test.js
+++ yo/models/test.js
@@ -1,13 +1,24 @@
-import connection from '../routes/controller';
+var mysql = require("mysql");
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'testuser',
+    password: 'fbehddls1',
+    database: 'test'
+});
+
 
 class test {
     constructor() {
         this.sql = "select * from test_table";
+        connection.query(this.sql, function(err, result) {
+            this.a = result;
+        });
 
     }
     showAll() {
-        connection.query(this.sql, function(err, result) {
-            console.log(result);
+        connection.query(sql, function(err, result) {
+            return result;
         });
     }
 }
--- yo/routes/controller.js
+++ yo/routes/controller.js
@@ -1,16 +1,12 @@
 var fs = require("fs");
-var mysql = require("mysql");
-var testsql = require("../models/test.js");
+var test_sql = require("../models/test");
 
-
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'testuser',
-    password: 'fbehddls1',
-    database: 'test'
-});
 
 exports.mainView = function(req, res) {
+    var tess = new test_sql();
+    console.log(typeof(tess));
+    //var a = tess.showAll();
+    //console.log(a);
     fs.readFile("./views/index.html", "utf-8", function(err, buf) {
         res.end(buf);
     });
